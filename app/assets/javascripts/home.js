cf7fcb3b2dcfa11d91318982a2752f7ee54b9764
Jarrett Ng <yewhong_ng91@hotmail.com>
2018-01-29 01:54:23 +0800

    first commit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,41 @@
+function moveToSelected(element) {
+
+  if (element == "next") {
+    var selected = $(".selected").next();
+  } else if (element == "prev") {
+    var selected = $(".selected").prev();
+  } else {
+    var selected = element;
+  }
+
+  var next = $(selected).next();
+  var prev = $(selected).prev();
+  var prevSecond = $(prev).prev();
+  var nextSecond = $(next).next();
+
+  $(selected).removeClass().addClass("selected");
+
+  $(next).removeClass().addClass("next");
+  $(prev).removeClass().addClass('hideLeft');
+
+  $(nextSecond).removeClass().addClass("nextRightSecond");
+  $(prevSecond).removeClass().addClass('hideLeft');
+
+  $(nextSecond).nextAll().removeClass().addClass('hideRight');
+  $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
+}
+
+$(document).on('turbolinks:load', function() {
+  $('#carousel div').click(function() {
+    moveToSelected($(this));
+  });
+
+  $('#prev').click(function() {
+    moveToSelected('prev');
+  });
+
+  $('#next').click(function() {
+    moveToSelected('next');
+  });
+})
